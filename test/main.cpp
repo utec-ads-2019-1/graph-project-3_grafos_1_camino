03d7b160dd2749384af72374ccc93f421900ddb5
TISparta <jose.garcia@utec.edu.pe>
2019-05-24 14:47:04 -0500

    Try-catch

--- test/main.cpp
+++ test/main.cpp
@@ -6,13 +6,17 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  cout << "Hola" << endl;
-  Graph <char, int>* graph = new Graph <char, int> (false);
-  graph -> addNode('a', 0.0, 0.0);
-  graph -> addNode('b', 1.1, 1.1);
-  graph -> addEdge('a', 'b', 4);
-  graph -> deleteEdge('a', 'b');
-  if (graph -> findNode('a')) cout << "a esta en el graphrafo" << endl;
-  delete graph;
+  try {
+    Graph <char, int>* graph = new Graph <char, int> (false);
+    graph -> addNode('a', 0.0, 0.0);
+    graph -> addNode('b', 1.1, 1.1);
+    graph -> addEdge('a', 'b', 4);
+    graph -> deleteEdge('a', 'b');
+    if (graph -> findNode('a')) cout << "a esta en el graphrafo" << endl;
+    graph -> Prim();
+    delete graph;
+  } catch (const char* msg) {
+    cerr << msg << endl;
+  }
   return EXIT_SUCCESS;
 }
